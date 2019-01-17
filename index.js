0ac5c9e386ecc668df3c72ffe9825f66c7515faf
Abby Gottlich <abby.gottlich@gmail.com>
2019-01-16 21:35:22 -0600

    mother function built

--- index.js
+++ index.js
@@ -7,34 +7,33 @@ const rl = readline.createInterface({
 });
 
 // mother of all functions
-// const mother = () => {
-//   rl.question("", answer => {
-//     let formattedAnswer = answer.split(" ");
-//     if (formattedAnswer[1] === "+" || "-" || "*" || "/") {
-//       calculator();
-//     } else if (formattedAnswer[1] === ">" || "<" || "=") {
-//       isComparisonAccurate();
-//     }
-//     mother();
-//   });
+const mother = () => {
+  rl.question("", answer => {
+    let formattedAnswer = answer.split(" ");
+    if (formattedAnswer[1] === "+" || "-" || "*" || "/") {
+      calculator(answer);
+    } else if (formattedAnswer[1] === ">" || "<" || "=") {
+      isComparisonAccurate(answer);
+    } else if (formattedAnswer[2] === "=") {
+      createVariable(answer);
+    }
+    mother();
+  });
+};
 
 // mother calculator function
-// const calculator = () => {
-//   rl.question("", answer => {
-//     let formattedAnswer = answer.split(" ");
-//     if (formattedAnswer[1] === "+") {
-//       addition(formattedAnswer);
-//     } else if (formattedAnswer[1] === "-") {
-//       subtraction(formattedAnswer);
-//     } else if (formattedAnswer[1] === "*") {
-//       multiplication(formattedAnswer);
-//     } else if (formattedAnswer[1] === "/") {
-//       division(formattedAnswer);
-//     }
-//     calculator();
-//   });
-// };
-// calculator();
+const calculator = answer => {
+  let formattedAnswer = answer.split(" ");
+  if (formattedAnswer[1] === "+") {
+    addition(formattedAnswer);
+  } else if (formattedAnswer[1] === "-") {
+    subtraction(formattedAnswer);
+  } else if (formattedAnswer[1] === "*") {
+    multiplication(formattedAnswer);
+  } else if (formattedAnswer[1] === "/") {
+    division(formattedAnswer);
+  }
+};
 
 // baby calculator functions
 function addition(formattedAnswer) {
@@ -54,20 +53,16 @@ function division(formattedAnswer) {
 }
 
 // mother boolean function
-// const isComparisonAccurate = () => {
-//   rl.question("", answer => {
-//     let formattedAnswer = answer.split(" ");
-//     if (formattedAnswer[1] === ">") {
-//       isGreaterThan(formattedAnswer);
-//     } else if (formattedAnswer[1] === "<") {
-//       isLessThan(formattedAnswer);
-//     } else if (formattedAnswer[1] === "=") {
-//       isEqualTo(formattedAnswer);
-//     }
-//     isComparisonAccurate();
-//   });
-// };
-// isComparisonAccurate();
+const isComparisonAccurate = answer => {
+  let formattedAnswer = answer.split(" ");
+  if (formattedAnswer[1] === ">") {
+    isGreaterThan(formattedAnswer);
+  } else if (formattedAnswer[1] === "<") {
+    isLessThan(formattedAnswer);
+  } else if (formattedAnswer[1] === "=") {
+    isEqualTo(formattedAnswer);
+  }
+};
 
 // baby boolean functions
 function isGreaterThan(formattedAnswer) {
@@ -95,18 +90,14 @@ function isEqualTo(formattedAnswer) {
 }
 
 // variable function
-const createVariable = () => {
-  rl.question("", answer => {
-    let formattedAnswer = answer.split(" ");
-    if (formattedAnswer[2] === "=") {
-      createdVariables[formattedAnswer[1]] = formattedAnswer[3];
-    }
-    if (formattedAnswer.length === 1 && createdVariables[formattedAnswer[0]]) {
-      console.log(createdVariables[formattedAnswer[0]]);
-    }
-    createVariable();
-  });
+const createVariable = answer => {
+  let formattedAnswer = answer.split(" ");
+  if (formattedAnswer[2] === "=") {
+    createdVariables[formattedAnswer[1]] = formattedAnswer[3];
+  }
+  if (formattedAnswer.length === 1 && createdVariables[formattedAnswer[0]]) {
+    console.log(createdVariables[formattedAnswer[0]]);
+  }
 };
-createVariable();
-
 const createdVariables = {};
+mother();
