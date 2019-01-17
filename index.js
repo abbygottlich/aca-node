0d0d08356bd3bddb89def37108d93fd5d4ba6776
Abby Gottlich <abby.gottlich@gmail.com>
2019-01-16 19:48:54 -0600

    calculator functions working

--- index.js
+++ index.js
@@ -5,84 +5,79 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let userInput = process.stdin;
+const calculator = () => {
+  rl.question("", answer => {
+    let formattedAnswer = answer.split(" ");
+    if (formattedAnswer[1] === "+") {
+      addition(formattedAnswer);
+    } else if (formattedAnswer[1] === "-") {
+      subtraction(formattedAnswer);
+    } else if (formattedAnswer[1] === "*") {
+      multiplication(formattedAnswer);
+    } else if (formattedAnswer[1] === "/") {
+      division(formattedAnswer);
+    }
+    calculator();
+  });
+};
+calculator();
 
 // calculator functions
-function addition(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (equation[1] === "+") {
-      console.log(parseInt(equation[0]) + parseInt(equation[2]));
-    }
-  }
+function addition(formattedAnswer) {
+  console.log(parseInt(formattedAnswer[0]) + parseInt(formattedAnswer[2]));
 }
 
-function subtraction(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (equation[1] === "-") {
-      console.log(parseInt(equation[0]) - parseInt(equation[2]));
-    }
-  }
+function subtraction(formattedAnswer) {
+  console.log(parseInt(formattedAnswer[0]) - parseInt(formattedAnswer[2]));
 }
 
-function multiplication(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (equation[1] === "*") {
-      console.log(parseInt(equation[0]) * parseInt(equation[2]));
-    }
-  }
+function multiplication(formattedAnswer) {
+  console.log(parseInt(formattedAnswer[0]) * parseInt(formattedAnswer[2]));
 }
 
-function division(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (equation[1] === "/") {
-      console.log(parseInt(equation[0]) / parseInt(equation[2]));
-    }
-  }
+function division(formattedAnswer) {
+  console.log(parseInt(formattedAnswer[0]) / parseInt(formattedAnswer[2]));
 }
 
-// boolean functions
-function isGreaterThan(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (equation[1] === ">" && parseInt(equation[0]) > parseInt(equation[2])) {
-      return true;
-      console.log(true);
-    } else {
-      return false;
-      console.log(false);
-    }
-  }
-}
+// // boolean functions
+// function isGreaterThan(userInput) {
+//   if (userInput.keyCode === 13) {
+//     let equation = userInput.split(" ");
+//     if (equation[1] === ">" && parseInt(equation[0]) > parseInt(equation[2])) {
+//       return true;
+//       console.log(true);
+//     } else {
+//       return false;
+//       console.log(false);
+//     }
+//   }
+// }
 
-function isLessThan(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (equation[1] === "<" && parseInt(equation[0]) < parseInt(equation[2])) {
-      return true;
-      console.log(true);
-    } else {
-      return false;
-      console.log(false);
-    }
-  }
-}
+// function isLessThan(userInput) {
+//   if (userInput.keyCode === 13) {
+//     let equation = userInput.split(" ");
+//     if (equation[1] === "<" && parseInt(equation[0]) < parseInt(equation[2])) {
+//       return true;
+//       console.log(true);
+//     } else {
+//       return false;
+//       console.log(false);
+//     }
+//   }
+// }
 
-function isEqualTo(userInput) {
-  if (userInput.keyCode === 13) {
-    let equation = userInput.split(" ");
-    if (
-      equation[1] === "=" &&
-      parseInt(equation[0]) === parseInt(equation[2])
-    ) {
-      return true;
-      console.log(true);
-    } else {
-      return false;
-      console.log(false);
-    }
-  }
-}
+// function isEqualTo(userInput) {
+//   if (userInput.keyCode === 13) {
+//     let equation = userInput.split(" ");
+//     if (
+//       equation[1] === "=" &&
+//       parseInt(equation[0]) === parseInt(equation[2])
+//     ) {
+//       return true;
+//       console.log(true);
+//     } else {
+//       return false;
+//       console.log(false);
+//     }
+//   }
+// }
